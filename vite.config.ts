dd94b7040c081b6f9e34a94b7f6201a53a5e3b0c
Eli Gibbs <eligibbs@outlook.com>
2026-02-06 12:30:34 -0500

    allow proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Expose to network
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const allowedHosts = env.ALLOWED_HOSTS ? env.ALLOWED_HOSTS.split(',') : []
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true, // Expose to network
+      allowedHosts: allowedHosts,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        }
       }
     }
   }
